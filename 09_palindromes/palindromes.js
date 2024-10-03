35c893d697b5d58d886751bac83c939a4e22045c
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-10-03 15:21:29 +0100

    Passed Test Suite

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alphaNumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const words = [...str.toLowerCase()].filter((item) =>
+    alphaNumerical.includes(item)
+  );
+  return words.join() === words.reverse().join();
 };
-
 // Do not edit below this line
 module.exports = palindromes;
