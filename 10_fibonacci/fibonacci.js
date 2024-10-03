8fb6e60df6632c3583af41652b14e93f17bb4e6a
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-10-03 15:58:02 +0100

    Pass Test Suite

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (+n < 0) return "OOPS";
+  if (+n === 0) return 0;
+  let n1 = 0;
+  let n2 = 1;
+  let result = 0;
+  for (let i = 2; i <= +n; i++) {
+    result = n1 + n2;
+    n1 = n2;
+    n2 = result;
+  }
+  console.log(result);
+  return n2;
 };
-
+fibonacci("4");
 // Do not edit below this line
 module.exports = fibonacci;
