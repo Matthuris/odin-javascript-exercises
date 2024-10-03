de31266bfc30aa59bb68c42aeac389f2bc1a7f64
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-10-03 13:29:40 +0100

    Completed Test Suite

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (...numbers) {
+  return numbers.reduce((sum, number) => (sum += number));
+};
+const subtract = function (...numbers) {
+  return numbers.reduce((sum, number) => (sum -= number));
 };
 
-const subtract = function() {
-	
+const sum = function (...numbers) {
+  if (numbers.flat().length < 1) return 0;
+  return numbers.flat().reduce((sum, number) => (sum += number));
+};
+const multiply = function (...numbers) {
+  return numbers.flat().reduce((sum, number) => (sum *= number));
+};
+const power = function (number, power) {
+  return number ** power;
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (number) {
+  let sum = number;
+  if (number === 0) return 1;
+  while (number > 1) {
+    number--;
+    sum *= number;
+  }
+  return sum;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
