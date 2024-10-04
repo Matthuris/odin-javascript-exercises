a90563e3669d85ddfa4f054c8563f788c4a67da4
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-10-04 10:53:31 +0100

    Complete all exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...remove) {
+  //   return arr.filter((val) => !remove.includes(val));
+  for (pos of remove) {
+    while (arr.includes(pos)) {
+      arr.splice(arr.indexOf(pos), 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (max, min) {
+  if (!Number.isInteger(max) || !Number.isInteger(min) || max < 0 || min < 0)
+    return "ERROR";
+  if (min > max) [max, min] = [min, max];
+  let result = 0;
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
